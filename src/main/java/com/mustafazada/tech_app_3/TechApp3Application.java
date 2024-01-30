5d391d0e5a570cb1ca85505b7215ba354cfeface
Ilkin <ilkinmustafazadeh@gmail.com>
2024-01-30 13:02:57 +0400

    firs commit

---
+++ src/main/java/com/mustafazada/tech_app_3/TechApp3Application.java
@@ -0,0 +1,13 @@
+package com.mustafazada.tech_app_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechApp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechApp3Application.class, args);
+    }
+
+}
