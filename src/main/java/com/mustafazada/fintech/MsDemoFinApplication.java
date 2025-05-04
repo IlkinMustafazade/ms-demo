d7117db4bd555aa03928011ce21f7e0db72cf98d
Ilkin <ilkinmustafazadeh@gmail.com>
2025-05-04 13:22:54 +0400

    first commit

---
+++ src/main/java/com/mustafazada/fintech/MsDemoFinApplication.java
@@ -0,0 +1,13 @@
+package com.mustafazada.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDemoFinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDemoFinApplication.class, args);
+    }
+
+}
